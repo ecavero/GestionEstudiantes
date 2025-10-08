b3a3adad758e7cdf22faf571dd54cd950db828ae
Eduardo Cavero <ecavero@gmail.com>
2025-10-07 21:35:04 -0500

    Crear el controlador Estudiante.

---
+++ src/main/java/com/isil/app/controller/EstudianteController.java
@@ -0,0 +1,25 @@
+package com.isil.app.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.isil.app.model.Estudiante;
+import com.isil.app.repository.EstudianteRepository;
+
+@Controller
+@RequestMapping("/estudiantes")
+public class EstudianteController {
+   @Autowired private EstudianteRepository estudianteRepository;
+
+   @GetMapping("/")
+   String listarEstudiantes(Model model) {
+      List<Estudiante> estudiantes = estudianteRepository.findAll();
+      model.addAttribute("estudiantes", estudiantes);
+      return "lista-estudiantes";
+   }
+}
